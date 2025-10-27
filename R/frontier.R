b23c6372b78a7962bc056055ea507c202599a7d3
Peter Winskill <p.winskill@imperial.ac.uk>
2025-10-27 14:06:00 +0000

    Fix up rule (cost saving now allowed

--- R/frontier.R
+++ R/frontier.R
@@ -164,7 +164,7 @@ build_frontier_up <- function(x, start_pos, threshold, up_filter) {
 
   repeat{
     # Get candidates that cost more AND have better impact
-    candidates <- x[x$cost >= current$cost & x$impact > current$impact, , drop = FALSE]
+    candidates <- x[x$impact > current$impact, , drop = FALSE]
     if (nrow(candidates) == 0) break
 
     # Apply user filter if provided
